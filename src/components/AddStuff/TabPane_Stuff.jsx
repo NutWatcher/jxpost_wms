a57e6fc3bd6b01c6e13df91cf366c35565c5c673
NutWatcher <lyyoicq@163.com>
2019-03-28 14:31:02 +0800

    新增物料

--- src/components/AddStuff/TabPane_Stuff.jsx
+++ src/components/AddStuff/TabPane_Stuff.jsx
@@ -1,28 +1,253 @@
 import React from 'react';
-import { Tabs, Icon, Button, message, Tag, Typography, Modal, Col, Row, Form, Input, Card } from 'antd';
+import { Tabs, Icon, Button, message, Tag, Typography, Modal, Col, Select, Form, Input, Table } from 'antd';
 import U_Fetch from '../../utils/fetchFilter';
 import './TabPane_Stuff.less';
-class TabPane_Stuff extends React.Component {
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+    }
+};
+class TabPane_Stuff_Form extends React.Component {
     state = {
-        markerPosition: {}
-    };
+        companyList: [],
+        isLoading: false
+    }
+    fetchAddSettlement = async (values) => {
+        this.setState({ isLoading: true });
+        try {
+            let stsFetch = new U_Fetch('/material', values, { method: 'POST' });
+            await stsFetch.queryFetch();
+            await stsFetch.filterFetch();
+            this.setState({ isLoading: false });
+            let data = stsFetch.data;
+            if (data.state !== true) {
+                message.error(`新增失败!:${data.message}` || '新增失败!');
+                return;
+            }
+            this.props.close();
+        } catch (e) {
+            message.error(`新增失败!:${e.toString()}` || '新增失败!');
+            this.setState({ isLoading: false });
+        }
+    }
     handleSubmit = (e) => {
-
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.fetchAddSettlement(values);
+            }
+        });
+    }
+    fetchDepartment = async () => {
+        this.setState({ isLoading: true });
+        try {
+            let params = {
+                query: '',
+                page: 0,
+                limit: 200
+            };
+            let stsFetch = new U_Fetch('/department', params);
+            await stsFetch.queryFetch();
+            await stsFetch.filterFetch();
+            this.setState({ isLoading: false });
+            let data = stsFetch.data;
+            if (data.state !== true) {
+                message.error(`获取公司列表失败!:${data.message}` || '获取公司列表失败!');
+                return;
+            }
+            this.setState({ companyList: data.rowList });
+        } catch (e) {
+            message.error(`获取公司列表失败!:${e.toString()}` || '获取上公司列表失败!');
+            this.setState({ isLoading: false });
+        }
     }
     componentDidMount () {
+        this.fetchDepartment();
     }
     render () {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div id="TabPane_ShopAddress">
-                <div className='content'>
-                    <div className='map'>
-                        <Map amapkey={'9621eeb601b439135ea21e377c371052'} zoom={14} events={this.amapEvents}>
+            <Form onSubmit={this.handleSubmit} {...formItemLayout} >
+                <Form.Item label="物料名称">
+                    {getFieldDecorator('name', {
+                        initialValue: this.props.name,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="物料编码">
+                    {getFieldDecorator('code', {
+                        initialValue: this.props.code,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="备注">
+                    {getFieldDecorator('misc', {
+                        initialValue: this.props.misc,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="型号">
+                    {getFieldDecorator('model', {
+                        initialValue: this.props.model,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="规格">
+                    {getFieldDecorator('specification', {
+                        initialValue: this.props.specification,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="单价">
+                    {getFieldDecorator('unitPrice', {
+                        initialValue: this.props.unitPrice,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="发票号">
+                    {getFieldDecorator('invoice', {
+                        initialValue: this.props.invoice,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Input size="large" placeholder="" />
+                    )}
+                </Form.Item>
+                <Form.Item label="结算公司">
+                    {getFieldDecorator('companyName', {
+                        initialValue: this.props.class,
+                        rules: [{ required: true, message: '不能为空' }]
+                    })(
+                        <Select size="large" optionFilterProp={'name'} placeholder="">
                             {
-                                this.state.markerPosition.longitude ? <Marker position={this.state.markerPosition} /> : null
+                                this.state.companyList.map((item, key) => {
+                                    return <Select.Option key={key} name={item.name} value={item.name}>{item.name}</Select.Option>;
+                                })
                             }
+                        </Select>
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    <Button size="large" type="primary" htmlType="submit" loading={this.state.isLoading}>新增</Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+const Wrap_TabPane_Stuff_Form = Form.create({ name: 'TabPane_Settlement_Form' })(TabPane_Stuff_Form);
 
-                        </Map>
-                    </div>
+const columns = [{
+    title: '编号',
+    dataIndex: 'id',
+    key: 'id'
+}, {
+    title: '名称',
+    dataIndex: 'name',
+    key: 'name'
+}, {
+    title: '条形码',
+    dataIndex: 'code',
+    key: 'code'
+}, {
+    title: '型号',
+    dataIndex: 'model',
+    key: 'model'
+}, {
+    title: '规格',
+    dataIndex: 'specification',
+    key: 'specification'
+}, {
+    title: '价格',
+    dataIndex: 'unitPrice',
+    key: 'unitPrice'
+}, {
+    title: '公司',
+    dataIndex: 'companyName',
+    key: 'companyName'
+}];
+class TabPane_Stuff extends React.Component {
+    state = {
+        isLoading: false,
+        visible: false,
+        data: []
+    }
+    showModal = () => {
+        this.setState({ visible: true });
+    }
+    close = () => {
+        this.setState({ visible: false });
+    }
+    addSuccess = () => {
+        this.close();
+        this.fetchStuff();
+    }
+    fetchStuff = async () => {
+        this.setState({ isLoading: true });
+        try {
+            let params = {
+                queryList: encodeURI(JSON.stringify([])),
+                page: 0,
+                limit: 200
+            };
+            let stsFetch = new U_Fetch('/material', params);
+            await stsFetch.queryFetch();
+            await stsFetch.filterFetch();
+            this.setState({ isLoading: false });
+            let data = stsFetch.data;
+            if (data.state !== true) {
+                message.error(`获取分类列表失败!:${data.message}` || '获取分类列表失败!');
+                return;
+            }
+            this.setState({ data: data.rowList });
+        } catch (e) {
+            message.error(`获取分类列表失败!:${e.toString()}` || '获取上分类列表失败!');
+            this.setState({ isLoading: false });
+        }
+    }
+    componentDidMount () {
+        this.fetchStuff();
+    }
+    render () {
+        const list = [];
+        this.state.data.forEach((item, key) => {
+            list.push({
+                key: key,
+                name: item.name,
+                id: item.id,
+                code: item.code,
+                model: item.model,
+                specification: item.specification,
+                companyName: item.companyName,
+                specificationc: item.specification,
+                unitPrice: item.unitPrice
+            });
+        });
+        return (
+            <div id="TabPane_Stuff">
+                <div className='content'>
+                    <Modal title="新增物料" visible={this.state.visible}
+                        onOk={this.close} onCancel={this.close}
+                        footer={null}>
+                        <Wrap_TabPane_Stuff_Form close={this.addSuccess}/>
+                    </Modal>
+                    <Button type="primary" htmlType="submit" onClick={this.showModal} className="add_button">新增物料</Button>
+                    <Table columns={columns} dataSource={list} loading={this.state.isLoading}/>
                 </div>
             </div>
         );
--- src/page/addStuff.jsx
+++ src/page/addStuff.jsx
@@ -4,6 +4,7 @@ import U_Fetch from '../utils/fetchFilter';
 import TabPane_Department from '../components/AddStuff/TabPane_Department.jsx';
 import TabPane_Settlement from '../components/AddStuff/TabPane_Settlement.jsx';
 import TabPane_Company from '../components/AddStuff/TabPane_Company.jsx';
+import TabPane_Stuff from '../components/AddStuff/TabPane_Stuff.jsx';
 import './addStuff.less';
 
 const { Text, Title } = Typography;
@@ -28,7 +29,7 @@ class AddStuff extends React.Component {
             <div id="AddStuff">
                 <div className='content'>
                     <Tabs defaultActiveKey="1" onChange={this.callback}>
-                        <TabPane tab="物料管理" key="1"><TabPane_Department /></TabPane>
+                        <TabPane tab="物料管理" key="1"><TabPane_Stuff /></TabPane>
                         <TabPane tab="机构部门管理" key="2"><TabPane_Department /></TabPane>
                         <TabPane tab="结算方式" key="3"><TabPane_Settlement /></TabPane>
                         <TabPane tab="结算公司" key="4"><TabPane_Company /></TabPane>
