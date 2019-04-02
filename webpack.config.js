62f63c45f5b782e65db2fad344d276bad7b65357
NutWatcher <lyyoicq@163.com>
2019-04-02 14:09:23 +0800

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['@babel/plugin-proposal-class-properties',
+                        plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime',
                             ['import', {
                                 'libraryName': 'antd',
                                 'libraryDirectory': 'es',
